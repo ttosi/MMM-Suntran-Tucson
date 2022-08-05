3a0e826c0896770bf4327f7fe9eac7bd612f0405
ttosi <ttosi519@gmail.com>
2022-08-04 18:07:26 -0700

    move datastore from csv to sqlite

--- MMM-Suntran-Tucson.js
+++ MMM-Suntran-Tucson.js
@@ -1,53 +1,12 @@
-// const fs = require("fs");
-// const CsvReader = require("csv-reader");
-// const { format } = require("date-fns");
-
-// let routeDataStream = fs.createReadStream("./data/suntran-route-4.csv", "utf8");
-// let routeData = [];
-
-// const bus = {
-//   getNext: (route, direction, day, time, limit) => {
-//     let index = -1;
-//     while (index === -1) {
-//       index = routeData.findIndex(
-//         (row) =>
-//           row[0] === route &&
-//           row[1] === direction &&
-//           row[2] === day &&
-//           row[3] === time
-//       );
-//       time++;
-//     }
-//     return routeData.slice(index, index + limit);
-//   },
-// };
-
-// routeDataStream
-//   .pipe(new CsvReader({ parseNumbers: true, trim: true }))
-//   .on("data", (row) => {
-//     routeData.push(row);
-//   })
-//   .on("end", () => {
-//     const now = new Date();
-//     console.log(now)
-//     const days = ["sun", "week", "week", "week", "week", "week", "sat"];
-//     const day = days[format(now, "i")];
-//     const time = format(now, "Hm");
-//     console.log(day, time)
-
-//     console.log(bus.getNext(4, "west", day, time, 5));
-//   });
-
-//---------------------------
-
-const routeCsvFile = "./data/suntran-routes.csv"
-const days = ["sun", "week", "week", "week", "week", "week", "sat"];
+const fs = require("fs").promises;
+const csv = require("async-csv");
+const { format } = require("date-fns");
 
 Module.register("MMM-Suntran-Tucson", {
   defaults: {
+    fadeSpeed: 4000,
     updateInterval: 60000,
     limit: 5,
-    fadeSpeed: 4000,
     weekStartTime: 500,
     weekEndTime: 2355,
     satStartTime: 615,
@@ -56,6 +15,8 @@ Module.register("MMM-Suntran-Tucson", {
     sunEndTime: 2030
   },
   routeData: [],
+  nextStopTimes: [],
+  days: ["sun", "week", "week", "week", "week", "week", "sat"],
   async start() {
     Log.info("Starting module: " + this.name);
 
@@ -64,9 +25,6 @@ Module.register("MMM-Suntran-Tucson", {
     this.routeData = await csv.parse(csvData);
     
     const currentTimes = getRouteTimes()
-  },
-  async getRouteTimes(route, direction, day, time, limit) {
-
   },
   loaded: function (callback) { },
   getDom: function () { },
--- playground.js
+++ playground.js
@@ -1,103 +1,108 @@
-const fs = require("fs").promises;
-const csv = require("async-csv");
+// const fs = require("fs").promises;
+// const csv = require("async-csv");
+const sqlite3 = require("sqlite3")
+const sqlite = require("sqlite");
 const { format } = require("date-fns");
-
-const days = ["sun", "week", "week", "week", "week", "week", "sat"];
 
 const suntran = {
     defaults: {
-        updateInterval: 60000,
-        fadeSpeed: 4000,
+        routes: [
+            { route: 4, stop: 11150 }
+        ],
+        limit: 5,
         weekStartTime: 500,
         weekEndTime: 2355,
         satStartTime: 615,
         satEndTime: 2135,
         sunStartTime: 730,
-        sunEndTime: 2030
+        sunEndTime: 2030,
+        fadeSpeed: 4000,
+        updateInterval: 60000
     },
+    days: ["sun", "week", "week", "week", "week", "week", "sat"],
     routeData: [],
-    nextStopTimes(data) {
+    nextStopTimes: [],
+    getNextStopTimes(data) {
         let index = -1;
         while (index === -1) {
             index = this.routeData.findIndex(
                 (row) =>
                     row[0] === data.route &&
-                    row[1] === data.direction &&
+                    row[1] === data.dir &&
                     row[2] === data.day &&
                     row[3] === data.time
             );
             data.time++;
         }
-        return this.routeData.slice(index, index + limit);
+
+        return this.routeData.slice(index, index + data.limit);
     },
     currentTime() {
         const now = new Date()
-        const day = days[format(now, "i")]
+        const day = this.days[format(now, "i")]
         const time = format(now, "HHmm")
 
-        const canRun = (this.defaults[day + "StartTime"] < time &&
-            this.defaults[day + "EndTime"] > time)
-
-        if (canRun) {
+        if (this.shouldRun(day, time)) {
             return {
                 day: day,
                 time: time
             }
         }
         return false;
+    },
+    shouldRun(day, time) {
+        return (this.defaults[day + "StartTime"] < time &&
+            this.defaults[day + "EndTime"] > time)
     }
 };
 
 (async () => {
-    const file = await fs.readFile("./data/suntran-route-4.csv", "utf8");
-    suntran.routeData = await csv.parse(file, {
-        cast: (val, ctx) => {
-            if (ctx.index === 0 || ctx.index === 3) return Number(val)
-            return val
-        },
+    const db = await sqlite.open({
+        filename: "./data/suntran.db",
+        driver: sqlite3.Database
     });
 
-    const run = suntran.currentTime()
-    console.log(run)
-    
-    
-    // suntran.nextStopTimes(
-    //     { route: 4, dir: "west", ...suntran.currentTime() }
-    // )
-    // if(stopTimes) {
-    //     console.log(stopTimes)
-    // }
+    const rows = await db.all(`
+        SELECT r.route_short_name, t.direction_id,
+            st.departure_time, r.route_color,
+            s.stop_id, s.stop_name
+        FROM routes r
+        JOIN trips t ON t.route_id = r.route_id
+        JOIN stop_times st ON st.trip_id = t.trip_id
+        JOIN stops s ON s.stop_id = st.stop_id
+        WHERE r.route_short_name = ${suntran.defaults.routes[0].route}
+        AND s.stop_id = ${suntran.defaults.routes[0].stop}
+        ORDER BY st.departure_time
+    `)
+
+    // console.log(suntran.routeData)
+    rows.map(r => {
+        suntran.routeData.push(
+            [ r.route_short_name, "west", "week", r.departure_time ]
+        )
+    });
+
+    // console.log(suntran.routeData)
+
+    // const file = await fs.readFile("./data/suntran-route-4.csv", "utf8");
+    // suntran.routeData = await csv.parse(file, {
+    //     cast: (val, ctx) => {
+    //         if (ctx.index === 0 || ctx.index === 3) return Number(val)
+    //         return val
+    //     },
+    // });
+
+    if (suntran.currentTime()) {
+        this.nextStopTimes = suntran.getNextStopTimes(
+            {
+                route: suntran.defaults.routes[0].route,
+                dir: "west",
+                // stop: suntran.defaults.routes[0].stop,
+                ...suntran.currentTime(),
+                limit: suntran.defaults.limit
+            }
+        )
+    }
+
+    console.log(this.nextStopTimes)
 })();
-
-// const bus = {
-//     getNext: (route, direction, day, time, limit) => {
-//         let index = -1;
-//         while (index === -1) {
-//             index = routeData.findIndex(
-//                 (row) =>
-//                     row[0] === route &&
-//                     row[1] === direction &&
-//                     row[2] === day &&
-//                     row[3] === time
-//             );
-//             time++;
-//         }
-//         return routeData.slice(index, index + limit);
-//     },
-// };
-
-// routeDataStream
-//     .pipe(new CsvReader({ parseNumbers: true, trim: true }))
-//     .on("data", (row) => {
-//         routeData.push(row);
-//     })
-//     .on("end", () => {
-//         const now = new Date();
-//         console.log(now)
-//         const days = ["sun", "week", "week", "week", "week", "week", "sat"];
-//         const day = days[format(now, "i")];
-//         const time = format(now, "Hm");
-//         console.log(day, time)
-
-//         console.log(bus.getNext(4, "west", day, time, 5));
-//     });
